27b9a7ae73672381889372f8c9a0a6254002811d
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-10-20 05:48:26 +0300

    update

--- Home_Work_4/src/app/app.component.ts
+++ Home_Work_4/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  users: string[] = ["Renzo","TheDarkReaper","DiSpaCe","PIPEC"]
+
   color: string = "желтый";
   login: string = "";
   test: string = "Вкл";
@@ -18,5 +20,37 @@ export class AppComponent {
   }
     isAdmin() {
     return this.login === "Renzo";
+  }
+
+  currentInfo: any;
+
+  carInfo = {
+    name: 'Audi R8',
+    size: 4.42,
+    speed: 330
+  };
+
+  planeInfo = {
+    name: 'Airbus A380',
+    size: 73,
+    speed: 945
+  };
+
+  shipInfo = {
+    name: 'Emma Maersk',
+    size: 397,
+    speed: 41
+  };
+
+  showCarInfo() {
+    this.currentInfo = this.carInfo;
+  }
+
+  showPlaneInfo() {
+    this.currentInfo = this.planeInfo;
+  }
+
+  showShipInfo() {
+    this.currentInfo = this.shipInfo;
   }
 }
